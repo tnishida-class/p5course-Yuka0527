3814671edb5e3eee35966d6ba1ec851945c2270c
Yuka0527 <fuyuno202052727lov@gmail.com>
2021-10-19 12:44:14 +0900

    チェッカー

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,32 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+if((i+j)%2==0){
+  fill(255,255,255);
+}
+else{
+  fill(120,120,120);
+}
+      rect(size*i,size*j,size,size);
     }
   }
+  for(let i = 0; i < 8; i++){
+    for(let j = 0; j < 8; j++){
+
+if((i+j)%2==0){
+  fill(255,255,255);
+}
+else if(j<3){
+  fill(255,0,0);
+}
+else if(j>4){
+  fill(0,0,0)
+}
+else{
+  fill(120,120,120)
+}
+ellipse(size*i+size/2,size*j+size/2,20)
+    }
+  }
+
 }
