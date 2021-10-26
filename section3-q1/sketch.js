3e74ce4fd1183e784357eaff516b701f54c60a35
Yuka0527 <fuyuno202052727lov@gmail.com>
2021-10-26 12:13:54 +0900

    心臓

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -10,7 +10,20 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  if(mouseIsPressed){
+  count = (count + 3) % cycle;
+}
+else{
+    count = (count + 1) % cycle;
+}
+
   // BLANK[1]
+  let size=count;
+  if(count<10){
   ellipse(width / 2, height / 2, size);
+}
+
+  else{
+    ellipse(width / 2, height / 2, 100-size)
+  }
 }
