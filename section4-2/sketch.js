978817f5ac20cde947abe67605aa78fc4bfbcc14
Yuka0527 <fuyuno202052727lov@gmail.com>
2021-11-08 11:27:46 +0900

    ランダムボール

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -7,6 +7,8 @@ let balls;
 function setup(){
   createCanvas(windowWidth, windowHeight);
   balls = [];
+  let b={size:random(10,1000)};
+  balls.push(b);
 }
 
 function draw(){
@@ -14,6 +16,7 @@ function draw(){
   for(let i = 0; i < balls.length; i++){
     let b = balls[i];
     ellipse(b.x, b.y, b.size);
+
     b.x += b.vx;
     b.y += b.vy;
   }
@@ -23,7 +26,7 @@ function mouseDragged(){
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
   if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const b = { x: mouseX, y: mouseY, size: random(10,100), vx: dx, vy: dy };
     balls.push(b);
   }
 }
